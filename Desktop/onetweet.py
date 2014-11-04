e9d03bb11f12145f3c9938dfd009774937ad94ad
sindunarasimhan <sindu@uw.edu>
2014-11-03 20:03:38 -0800

    Delete onetweet.py

--- Desktop/onetweet.py
+++
@@ -1,4 +0,0 @@
-import sys
-import re
-pat = re.compile(r'\B#\w+')
-print(pat.findall('RT @assholeofday: #Ebola: Rick Wiles hopes it will "cleanse" America of atheism, homosexuality and promiscuity http://t.co/lUPP805PvK http:\u2026'))
